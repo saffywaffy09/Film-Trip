a5579551b5867391f09095010601552ebca8ceaa
Safwan Ahmed <60561100+saffywaffy09@users.noreply.github.com>
2025-05-22 21:03:55 +0000

    database

--- src/index.js
+++ src/index.js
@@ -2,16 +2,31 @@ import fs from "fs";
 import Papa from "papaparse";
 import axios from "axios";
 import * as cheerio from "cheerio";
+import sqlite3 from "sqlite3";
 
-const csvFile = fs.readFileSync('public/movies.csv', 'utf8');
+const db = new sqlite3.Database('./movies.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the database.');
+});
+
+const csvFile = fs.readFileSync('public/top_100_movies.csv', 'utf8');
 const file = Papa.parse(csvFile);
 
-const info = file.data.map(row => ({
-    "id": row[0],
-    "movieName": row[1]
-}));
 
-async function getMovieLocations (movieId) {
+db.exec(`
+    CREATE TABLE IF NOT EXISTS locations(movieName, movieId, locationString, lat, lon, UNIQUE(movieName, movieId, locationString, lat, lon));
+    `);
+
+// db.exec(`DELETE FROM locations;`);
+
+console.log("RIGHT HERE");
+const info = file.data.map(row => ({
+    "id": row[1],
+    "movieName": row[5]
+}));
+async function getMovieLocations (movieName, movieId) {
     let response;
     try {
 
@@ -34,6 +49,9 @@ async function getMovieLocations (movieId) {
     const locations = [];
     $('a.ipc-link.ipc-link--base.sc-781c1bad-2.jZxGAW').each((i, elem) => {
         let text = $(elem).text().trim();
+        let command = `INSERT INTO locations(movieName, movieId, locationString)
+                 VALUES("${movieName}", "${movieId}", "${text}")`;
+        db.exec(command);
         locations.push(text);
     });
     return locations;
@@ -59,27 +77,46 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-let locationResults = await getMovieLocations('tt0418279');
 
-locationResults = locationResults.map(loc => {
-    const afterDash = loc.replace(/^.*-\s*/, '');
+// locationResults = locationResults.map(loc => {
+//     const afterDash = loc.replace(/^.*-\s*/, '');
 
-    return afterDash
-        .replace(/,/g, '')     // remove commas
-        .split(/\s+/)          // split by any whitespace
-        .filter(Boolean)       // remove empty strings
-        .join('+');     
+//     return afterDash
+//         .replace(/,/g, '')     // remove commas
+//         .split(/\s+/)          // split by any whitespace
+//         .filter(Boolean)       // remove empty strings
+//         .join('+');     
+// });
+
+// console.log("SHAWSHANK REDEMPTION")
+// console.log(locationResults.length);
+// // console.log(locationResults);
+// for (let i = 0; i < locationResults.length; i++) {
+//     console.log(locationResults[i]);
+//     let geocodeResults = await getCoordinates(locationResults[i]); 
+//     console.log(geocodeResults);
+//     await sleep(1000);
+//     // if (geocodeResults) {
+//     //     console.log(`Location: ${locationResults[i]} and latitude ${geocodeResults.latitude} and longitude ${geocodeResults.longitude}`);
+//     // }
+// }
+
+
+// for (let i = 1; i < info.length; i++) {
+//     await getMovieLocations(info[i].movieName, info[1].id);
+// }
+
+
+
+db.all('SELECT * FROM locations', [], async (err, rows) => {
+        for (let i = 0; i < 1; i++) {
+        let loc = rows[i].locationString.replace(/^.*-\s*/, '').replace(/,/g, '')     // remove commas
+                .split(/\s+/)          // split by any whitespace
+                .filter(Boolean)       // remove empty strings
+                .join('+');     
+        console.log(loc);
+        let geocodeResults = await getCoordinates(loc);
+        db.exec(`UPDATE locations SET lat=${geocodeResults.latitude} long=${geocodeResults.longitude} WHERE locationString=${rows[i].locationString} `)
+        console.log(geocodeResults);
+    }
 });
-
-console.log("SHAWSHANK REDEMPTION")
-console.log(locationResults.length);
-// console.log(locationResults);
-for (let i = 0; i < locationResults.length; i++) {
-    console.log(locationResults[i]);
-    let geocodeResults = await getCoordinates(locationResults[i]); 
-    console.log(geocodeResults);
-    await sleep(1000);
-    // if (geocodeResults) {
-    //     console.log(`Location: ${locationResults[i]} and latitude ${geocodeResults.latitude} and longitude ${geocodeResults.longitude}`);
-    // }
-}
